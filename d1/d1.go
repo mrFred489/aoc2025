71f913fdbf2cbe1b4d83d685e6d30365285e2692
Frederik Aarup Lauritsen <me@frederikal.dk>
2025-12-01 21:48:43 +0100

    Add a helpers class and rewrite d1

--- d1/d1.go
+++ d1/d1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"aoc2025/helpers"
 	"bufio"
 	"fmt"
 	"log"
@@ -8,8 +9,8 @@ import (
 	"strconv"
 )
 
-func main() {
-	file, err := os.Open("input.txt")
+func Solve(fileName string, first bool) int {
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -21,7 +22,7 @@ func main() {
 	password2 := 0
 	for scanner.Scan() {
 		text := scanner.Text()
-		fmt.Println(text)
+		helpers.LogLine(text)
 		direction := string(text[0])
 		amount, err := strconv.Atoi(string(text[1:]))
 
@@ -34,7 +35,7 @@ func main() {
 			// fmt.Println(amount)
 			// fmt.Println(amount / 100)
 			password2 += amount / 100
-			fmt.Println(password2)
+			helpers.LogLine(password2)
 		}
 
 		amount = amount % 100
@@ -43,36 +44,76 @@ func main() {
 			dial += amount
 			if dial >= 100 {
 				password2 += 1
-				fmt.Println(password2)
+				helpers.LogLine(password2)
 			}
 			dial = dial % 100
 		} else {
 			if dial-amount < 0 {
 				if dial != 0 {
 					password2 += 1
-					fmt.Println(password2)
+					helpers.LogLine(password2)
 				}
 				dial = 100 + (dial - amount)
 			} else {
 				dial -= amount
 				if dial == 0 {
 					password2 += 1
-					fmt.Println(password2)
+					helpers.LogLine(password2)
 				}
 			}
 		}
-		// 	fmt.Println(direction)
-		// 	fmt.Println(amount)
-		// 	fmt.Println(dial)
 		if dial == 0 {
 			password += 1
 		}
 	}
 
-	fmt.Println(password)
-	fmt.Println(password2)
+	helpers.LogLine(password)
+	helpers.LogLine(password2)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	if first {
+		return password
+	} else {
+		return password2
+	}
+}
+
+func P1() {
+	tests := []helpers.Testcase{
+		{FileName: "sample1.txt", ExpectedResult: 3},
+		{FileName: "sample2.txt", ExpectedResult: 0},
+	}
+
+	errors := helpers.RunTests(tests, Solve, true)
+
+	if errors != 0 {
+		log.Fatal("Had errors, not running main input")
+		return
+	}
+
+	fmt.Printf("P1: %d\n", Solve("input.txt", true))
+}
+
+func P2() {
+	tests := []helpers.Testcase{
+		{FileName: "sample1.txt", ExpectedResult: 6},
+		{FileName: "sample2.txt", ExpectedResult: 11},
+	}
+
+	errors := helpers.RunTests(tests, Solve, false)
+
+	if errors != 0 {
+		log.Fatal("Had errors, not running main input")
+		return
+	}
+
+	fmt.Printf("P2: %d\n", Solve("input.txt", false))
+}
+
+func main() {
+	//P1()
+	P2()
 }
---
+++ helpers/helpers.go
@@ -0,0 +1,32 @@
+package helpers
+
+import (
+	"fmt"
+	"log"
+)
+
+type Testcase struct {
+	FileName       string
+	ExpectedResult int
+}
+
+func LogLine(a ...any) {
+	fmt.Println(a...)
+}
+
+type Solver func(fileName string, first bool) int
+
+func RunTests(tests []Testcase, method Solver, versionIndicator bool) int {
+	errors := 0
+
+	for index := range tests {
+		testcase := tests[index]
+		result := method(testcase.FileName, versionIndicator)
+		if result != testcase.ExpectedResult {
+			log.Fatal("Test failed", testcase, result)
+			errors += 1
+		}
+	}
+
+	return errors
+}
